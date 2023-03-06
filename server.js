b787f5c8196b4b37b12a1145b6be09b0550080e1
Baranin67 <mati.baranowicz@gmail.com>
2023-03-06 19:17:04 +0100

    Improved performance
    Decreased image loading time by compressing them.

--- server.js
+++ server.js
@@ -16,21 +16,12 @@ app.use('/scripts',
         { setHeaders: (res, path, stat) => { res.set('Content-type', 'application/javascript'); } }));
 
 app.use('/styles/out',
-    express.static(path.join(__dirname, 'styles/out'),
+    express.static(path.join(__dirname, 'styles', 'out'),
         { setHeaders: (res, path, stat) => { res.set('Content-type', 'text/css'); } }));
 
 app.use('/',
     express.static(path.join(__dirname, 'pages'),
         { setHeaders: (res, path, stat) => { res.set('Content-type', 'text/html'); } }));
-
-const configuration = new Configuration({
-    organization: "org-6RaN2b0V2177llavuyry5oin",
-    apiKey: process.env.OPENAI_API_KEY
-});
-const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
-
-app.get('');
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
